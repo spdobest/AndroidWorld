1c9b8174ecc15cf28ace1e68f45404cf4f8ff70f
spdobest <sp.dobest@gmail.com>
2020-05-26 09:42:35 +0100

    Added Coroutines and adapters to implement network call

---
+++ app/src/main/java/spm/androidworld/all/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,13 @@
+package spm.androidworld.all.coroutines
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import spm.androidworld.all.R
+
+class CoroutinesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines)
+    }
+}
